b4e5a8bf15a8dec4a9515d4e42653535088ddfe7
Chia-Yu Sun <chiayusun.cs08g@nctu.edu.tw>
2020-06-11 17:10:48 +0800

    feat(readme, sample): update README, sample code

--- sample.py
+++ sample.py
@@ -5,29 +5,20 @@ from rule_classifier import RuleClassifier
 # read training data
 readers = [XMLReader(f"Logs/Train/Person_{i}/Security.xml", f"Logs/Train/Person_{i}/Sysmon.xml") for i in range(1, 7)]
 input_data = [(i, readers[i-1]) for i in range(1, 7)]
-rclf = RuleClassifier(input_data)
 
-# testing
-## read testing data
+# read testing data
 test_1 = XMLReader('Logs/Example Test/Test_1/Security.xml', 'Logs/Example Test/Test_1/Sysmon.xml')
 
-clf_1 = RFClassifier(train_data=[reader.dataframe for reader in readers], test_data=test_1.dataframe)
+# build rule classifier
+rule_clf = RuleClassifier(input_data)
+
+# build random forest classifier
+rf_clf = RFClassifier(train_data=[reader.dataframe for reader in readers], test_data=test_1.dataframe)
+
+# predict results
 print("\nTest 1\n")
+print("# ML classifier")
+print(rf_clf.predict()[0])
 
-print("ML classifier")
-print(clf_1.predict()[0])
-
-print("Rule classifier")
-print(rclf.predict(test_1))
-
-test_2 = XMLReader('Logs/Example Test/Test_2/Security.xml', 'Logs/Example Test/Test_2/Sysmon.xml')
-clf_2 = RFClassifier(train_data=[reader.dataframe for reader in readers], test_data=test_2.dataframe)
-
-print("\nTest 2\n")
-print("ML classifier")
-print(clf_2.predict()[0])
-
-print("Rule classifier")
-print(rclf.predict(test_2))
-
-
+print("\n# Rule classifier")
+print(rule_clf.predict(test_1))
